d40a6f80de7dcf701451949cdedd56e048c07bc2
taslimisina <taslimisina@yahoo.com>
2021-02-19 23:04:35 +0330

    Added Map scripts and scene

---
+++ Assets/Scripts/Map/GameManager.cs
@@ -0,0 +1,16 @@
+﻿public class GameManager
+{
+    private NewBehaviourScript gameLog;   //Todo change class to GameLog(It's not created yet)
+    private int currTurn;
+
+    public GameManager(NewBehaviourScript gameLog)
+    {
+        this.gameLog = gameLog;
+    }
+
+    public void applyLog(int turn)
+    {
+        this.currTurn = turn;
+        // Todo
+    }
+}
