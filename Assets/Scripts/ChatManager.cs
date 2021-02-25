d7f91d7515e4d4cea61969458f0cdd0452cfc80c
SMahdiFaghih <smahdifaghih2001@gmail.com>
2021-02-25 15:03:48 +0330

    added ChatManager

---
+++ Assets/Scripts/ChatManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text[] Texts;
+    private int TextsCount;
+
+    private void Start()
+    {
+        TextsCount = Texts.Length;
+    }
+
+    public void SetChatMessages(string[] messages)
+    {
+        int messagesCount = messages.Length;
+        if (messagesCount >= TextsCount)
+        {
+            for (int i = 0; i < TextsCount ; i++)
+            {
+                Texts[i].text = messages[messagesCount - TextsCount + i];
+            }
+        }
+        else
+        {
+            for (int i = 0; i < TextsCount - messagesCount; i++)
+            {
+                Texts[i].text = Texts[i + messagesCount].text;
+            }
+            for (int i = TextsCount - messagesCount; i < TextsCount; i++)
+            {
+                Texts[i].text = messages[messagesCount - TextsCount + i];
+            }
+        }
+    }
+}
